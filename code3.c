e90274564c564c91b0e0f199942266b7ffb0d562
序属秋秋秋 <3525035846@qq.com>
2025-09-01 18:30:57 +0800

    展示行缓冲

---
+++ code3.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: code3.c
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 01 Sep 2025 05:01:05 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("hello world");
+    fflush(stdout);
+    sleep(3);
+    return 0;
+}
