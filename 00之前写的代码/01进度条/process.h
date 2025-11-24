815ba0041ac368ffb4239dae399d5fda3e048c29
序属秋秋秋 <3525035846@qq.com>
2025-11-24 11:09:12 +0800

    将之前写的代码重新挪动到新的位置

--- 00之前写的代码/01进度条/process.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-//1.声明 version1 版本的进度条函数
-//2.声明 version2 版本的进度条函数 ---> 区别之处：接收“总数据量”和“当前已处理数据量参数”
-
-void process_v1();
-void process_v2(double total, double current);
